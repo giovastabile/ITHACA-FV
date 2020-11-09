238307498d6d5167546823daa2702e71a806eb16
fromor <francesco.romor@gmail.com>
2020-11-09 17:27:02 +0100

    add burgers
    add openFoam burgers solver, add burgers1D and burgers2D testcases

---
+++ src/ITHACA_ROMPROBLEMS/ReducedBurgers/burgersFoam.C
@@ -0,0 +1,83 @@
+/*---------------------------------------------------------------------------*\
+Application
+    burgersFoam
+
+Description
+    Burgers equation solver.
+
+    \heading Solver details
+    The equation is given by:
+
+    \f[
+        \ddt{T}  = \div \left( D_T \grad T \right)
+    \f]
+
+    Where:
+    \vartable
+        T     | Scalar field which is solved for, e.g. temperature
+        D_T   | Diffusion coefficient
+    \endvartable
+
+    \heading Required fields
+    \plaintable
+        T     | Scalar field which is solved for, e.g. temperature
+    \endplaintable
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvOptions.H"
+#include "simpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Laplace equation solver for a scalar quantity."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    simpleControl simple(mesh);
+
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating vector transport\n" << endl;
+
+    #include "CourantNo.H"
+
+    while (simple.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+        Info<< nu << endl;
+
+        while (simple.correctNonOrthogonal())
+        {
+            fvVectorMatrix UEqn
+            (
+                fvm::ddt(U)
+              + fvm::div(phi, U)
+	    ==
+                fvm::laplacian(nu, U)
+            );
+            UEqn.solve();
+            phi = linearInterpolate(U) & mesh.Sf();
+        }
+
+        runTime.write();
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+// ************************************************************************* //
